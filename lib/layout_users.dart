c8cdeadf38cc789262e4cdd0362a068047373fb6
joelberzalgithub <120808626+joelberzalgithub@users.noreply.github.com>
2024-03-01 16:08:27 +0100

    Afegida nova finestra d'usuaris

---
+++ lib/layout_users.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LayoutUsers extends StatefulWidget {
+  const LayoutUsers({Key? key}) : super(key: key);
+
+  @override
+  LayoutUserState createState() => LayoutUserState();
+}
+
+class LayoutUserState extends State<LayoutUsers> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
